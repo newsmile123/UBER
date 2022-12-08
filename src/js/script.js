73d44dd0cf923436651108a41abde29390581d4f
Vasily Agafonov <vasily.agafonov@bk.ru>
2022-12-08 12:30:29 +0300

    add

--- src/js/script.js
+++ src/js/script.js
@@ -24,7 +24,8 @@ const ssdnewBranch32 = "branch test 23";
 const newBranch = "branch test 1";
 
 const newBranch2 = "branch test 2";
-
+const testwerwer = "";
+const newtestetwerew = "";
 const newBranch32 = "branch test 23";
 
 const ssdnewBranch32 = "branch test 23";
